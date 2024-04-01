18e16ee8cad26d4394c35d5592fe02c2ce4a95bb
P0203K <preeti.khamkar62@gmail.com>
2024-04-01 19:56:53 +0530

    final

--- app.py
+++ app.py
@@ -1,89 +1,4 @@
 
-# from flask import Flask, render_template, request, redirect, url_for, session
-
-# app = Flask(__name__)
-# app.secret_key = 'super_secret_key'
-
-# quizzes = {
-#     'number': {
-#         'questions': ['/1/1.jpg', '/2/1.jpg', '/3/1.jpg', '/4/1.jpg','/5/2.jpg', '/6/1.jpg', '/7/1.jpg', '/8/1.jpg', '/9/1.jpg',  ],
-#         'answers': ['1', '2', '3', '4', '5', '6', '7', '8', '9'],  
-#         'options': [
-#             ['1', '5', '2', '7'],
-#             ['3', '2', '4', '1'],
-#             ['1', '6', '3', '7'],
-#             ['0', '9', '4', '2'],
-#             ['5', '6', '3', '8'],
-#             ['4', '2', '6', '9'],
-#             ['2', '3', '8', '7'],
-#             ['5', '3', '8', '4'],
-#             ['6', '9', '2', '7'],
-#         ]
-#     },
-#     'alphabet': {
-#         'questions': ['/A/1.jpg', '/B/1.jpg'], 
-#         'answers': ['C', 'A'],  
-#         'options': [
-#             ['Option 1A', 'Option 1B', 'Option 1C', 'Option 1D'],
-#             ['Option 2A', 'Option 2B', 'Option 2C', 'Option 2D']
-#         ]
-#     }
-# }
-
-# @app.route('/')
-# def home():
-#     return render_template('index.html')
-
-# @app.route('/quiz')
-# def render_quiz():
-#     return render_template('quiz.html')
-
-# @app.route('/quiz/<category>/<int:question_num>', methods=['GET', 'POST'])
-# def quiz(category, question_num):
-#     if request.method == 'POST':
-#         app.logger.info('Form Data: %s', request.form)
-#         selected_option = request.form.get('option')  # Access the selected option directly
-#         app.logger.info('Selected Option: %s', selected_option)
-
-#         # Storing selected options in session
-#         selected_options = session.get('selected_options', {})
-#         selected_options[question_num] = selected_option
-#         session['selected_options'] = selected_options
-
-#         if selected_option == quizzes[category]['answers'][question_num - 1]:
-#             session['score'] = session.get('score', 0) + 1
-
-#         if question_num < len(quizzes[category]['questions']):
-#             return redirect(url_for('quiz', category=category, question_num=question_num + 1))
-#         else:
-#             return redirect(url_for('result', category=category))
-
-#     question = quizzes[category]['questions'][question_num - 1]
-#     options = quizzes[category]['options'][question_num - 1]
-#     return render_template('mcq.html', category=category, question_num=question_num, question=question, options=options)
-
-# @app.route('/result/<category>')
-# def result(category):
-#     score = session.get('score', 0)
-#     total_questions = len(quizzes[category]['questions'])
-#     session.pop('score', None)
-#     session.pop('selected_options', None)
-#     return render_template('result.html', score=score, total_questions=total_questions, category=category)
-
-# @app.route('/review/<category>')
-# def review(category):
-#     questions = quizzes[category]['questions']
-#     answers = quizzes[category]['answers']
-#     options = quizzes[category]['options']
-#     enumerated_questions = list(enumerate(questions, start=1))
-
-#     # Get the selected options from session if available
-#     selected_options = session.get('selected_options', {})
-
-#     return render_template('review.html', category=category, enumerated_questions=enumerated_questions, answers=answers, options=options, selected_options=selected_options)
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
 import os
 import mediapipe as mp
 import cv2
@@ -97,6 +12,7 @@ from flask import Flask, render_template, Response, request, redirect, url_for, 
 # Initialize Flask app
 app = Flask(__name__)
 app.secret_key = 'super_secret_key'
+
 
 # Load the machine learning model
 model_dict = pickle.load(open('./model_4.p', 'rb'))
@@ -217,22 +133,17 @@ quizzes = {
     }
 }
 
+user_answers = []  # Store user's answers for review
+
 @app.route('/quiz')
 def render_quiz():
-    return render_template('quiz.html')
+    return render_template('home.html')
 
 @app.route('/quiz/<category>/<int:question_num>', methods=['GET', 'POST'])
 def quiz(category, question_num):
     if request.method == 'POST':
-        app.logger.info('Form Data: %s', request.form)
-        selected_option = request.form.get('option')  # Access the selected option directly
-        app.logger.info('Selected Option: %s', selected_option)
-
-        # Storing selected options in session
-        selected_options = session.get('selected_options', {})
-        selected_options[question_num] = selected_option
-        session['selected_options'] = selected_options
-
+        selected_option = request.form['option']
+        user_answers.append({'user_answer': selected_option})
         if selected_option == quizzes[category]['answers'][question_num - 1]:
             session['score'] = session.get('score', 0) + 1
 
@@ -243,27 +154,45 @@ def quiz(category, question_num):
 
     question = quizzes[category]['questions'][question_num - 1]
     options = quizzes[category]['options'][question_num - 1]
-    return render_template('mcq.html', category=category, question_num=question_num, question=question, options=options)
+    return render_template('quiz.html', category=category, question_num=question_num, question=question, options=options)
 
 @app.route('/result/<category>')
 def result(category):
     score = session.get('score', 0)
     total_questions = len(quizzes[category]['questions'])
-    session.pop('score', None)
-    session.pop('selected_options', None)
+    session.pop('score', None)  
     return render_template('result.html', score=score, total_questions=total_questions, category=category)
 
-@app.route('/review/<category>')
+@app.route('/review/<category>', methods=['GET','POST'])
 def review(category):
+    print(user_answers)
+    review_data = []
+    for item in user_answers:
+    #     question_num = item['question_num']
+        user_answer=item['user_answer']
+    #     answers = quizzes[category]['answers']
+    #     questions = quizzes[category]['questions']
+    #     enumerated_questions = list(enumerate(questions, start=1))
+        review_data.append({'user_answer':user_answer})
+    print(review_data)
+    # return render_template('review.html', review_data=review_data, category=category)
+
     questions = quizzes[category]['questions']
     answers = quizzes[category]['answers']
     options = quizzes[category]['options']
     enumerated_questions = list(enumerate(questions, start=1))
 
-    # Get the selected options from session if available
-    selected_options = session.get('selected_options', {})
+    combined_data = []
+    for index, question in enumerate(enumerated_questions):
+        combined_data.append({
+            'question': question[1],
+            'review_item': review_data[index],
+            'correct_answer': answers[index]
+    })
+    print(combined_data)
+    return render_template('review.html', category=category, combined_data=combined_data, answers=answers, options=options, review_data=review_data)
 
-    return render_template('review.html', category=category, enumerated_questions=enumerated_questions, answers=answers, options=options, selected_options=selected_options)
+
 
 if __name__ == '__main__':
     cap = cv2.VideoCapture(0)
